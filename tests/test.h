0a09bdcadf46c2a03eca3ce45a17829c530a4167
Adrian Tudev <tudev@chalmers.se>
2025-11-24 21:59:32 +0100

    unit testing of utility function split_string

--- tests/test.h
+++ tests/test.h
@@ -4,20 +4,23 @@
 #include <assert.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <string.h>
 
 #include "utils/string_utils.h"
 
 void runTests() {
-  const char* str = "this is a string";
+  const char* str = "  this is  a    string  ";
+  const char* gold[] = {"this", "is", "a", "string"};
 
-  int len = -1;
+  size_t len = -1;
   const char** splitted = split_string(str, &len, NULL);
-
   assert(len == 4);
-  for (int i = 0; i < len; i++) {
-    printf("%s ", splitted[i]);
+
+  for (size_t i = 0; i < len; i++) {
+    assert(strcmp(splitted[i], gold[i]) == 0);
+    printf("|%s| ", splitted[i]);
   }
-  printf("\n");
+  printf("\nTests passed!\n");
 }
 
 #endif // TEST_H_
