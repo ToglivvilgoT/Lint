3d1ea1605560445b9bb2fd1db5e4d4a2ccf69e3f
Adrian Tudev <tudev@chalmers.se>
2025-11-26 14:45:41 +0100

    tokenizing literals

--- src/token.c
+++ src/token.c
@@ -10,9 +10,9 @@ static void read_operator(Token* token, const char* line, int pos);
 static void read_punctuation(Token* token, const char* line, int pos);
 static bool char_in_arr(const char c, const char* arr);
 
+static const char* TokenType_names[];
 static const char operators[] = {'+', '-', '/', '*', '=', '!', '>', '<'};
 static const char punctuators[] = {'(', ')', '{', '}', ',',};
-
 
 /*
  * Tokenize checks searches for tokens in the following order:
@@ -57,15 +57,13 @@ static inline char peek_next(const char* s, int pos) {
 }
 
 // searches for keywords and identifiers
-static void read_word(Token* tok, const char* line, int pos) {
-  size_t i = pos;
+static void read_word(Token* tok, const char* line, int linePos) {
+  size_t i = linePos;
 
   while (isalnum(peek_next(line, i)) || peek_next(line, i) == '_') i++;
 
-  int len = i - pos + 1;
-  char* token_str = malloc(len + 1);
-  memcpy(token_str, line + pos, len);
-  token_str[len] = '\0';
+  int len = i - linePos + 1;
+  char* token_str = substring(line, linePos, len);
 
   // use lookup table instead?
   if      (strcmp(token_str, "fn") == 0)    tok->type = FUNCTION;
@@ -77,6 +75,7 @@ static void read_word(Token* tok, const char* line, int pos) {
   else if (strcmp(token_str, "false") == 0) tok->type = FALSE;
   else if (strcmp(token_str, "and") == 0)   tok->type = AND;
   else if (strcmp(token_str, "or") == 0)    tok->type = OR;
+  else if (strcmp(token_str, "not") == 0)   tok->type = NOT;
   else if (strcmp(token_str, "if") == 0)    tok->type = IF;
   else if (strcmp(token_str, "elif") == 0)  tok->type = ELIF;
   else if (strcmp(token_str, "else") == 0)  tok->type = ELSE;
@@ -86,7 +85,26 @@ static void read_word(Token* tok, const char* line, int pos) {
 }
 
 static void read_literal(Token* tok, const char* line, int linePos) {
-  tok->type = INVALID;
+  size_t i = linePos;
+
+  while (isdigit(peek_next(line, i)) || peek_next(line, i) == '.') i++;
+
+  size_t len = i - linePos + 1;
+  char* token_str = substring(line, linePos, len);
+
+  uint32_t dots = 0;
+  for (size_t j = 0; j < len; j++) {
+    if (token_str[j] == '.') dots++;
+  }
+
+  if (dots > 1) {
+    tok->type = INVALID;
+    fprintf(stderr, "Error while reading literal (too many dots)");
+    return;
+  } 
+
+  tok->token = token_str;
+  tok->type = LITERAL;
 }
 
 static void read_string(Token* tok, const char* line, int linePos) {
@@ -110,53 +128,15 @@ static bool char_in_arr(const char c, const char* arr) {
 }
 
 static const char* token_type_name(TokenType type) {
-    switch (type) {
-        case LEFT_PARENTHESIS:   return "LEFT_PARENTHESIS";
-        case RIGHT_PARENTHESIS:  return "RIGHT_PARENTHESIS";
-        case LEFT_BRACE:         return "LEFT_BRACE";
-        case RIGHT_BRACE:        return "RIGHT_BRACE";
-        case MINUS:              return "MINUS";
-        case PLUS:               return "PLUS";
-        case STAR:               return "STAR";
-        case SLASH:              return "SLASH";
-        case SEMICOLON:          return "SEMICOLON";
-        case QUOTES:             return "QUOTES";
-        case COMMA:              return "COMMA";
-        case DOT:                return "DOT";
+  return TokenType_names[type];
+}
 
-        case BANG:               return "BANG";
-        case BANG_EQUAL:         return "BANG_EQUAL";
-
-        case EQUAL:              return "EQUAL";
-        case EQUAL_EQUAL:        return "EQUAL_EQUAL";
-
-        case GREATER:            return "GREATER";
-        case GREATER_EQUAL:      return "GREATER_EQUAL";
-
-        case LESS:               return "LESS";
-        case LESS_EQUAL:         return "LESS_EQUAL";
-
-        case IDENTIFIER:         return "IDENTIFIER";
-        case STRING:             return "STRING";
-        case NUMBER:             return "NUMBER";
-
-        case AND:                return "AND";
-        case OR:                 return "OR";
-        case IF:                 return "IF";
-        case ELIF:               return "ELIF";
-        case ELSE:               return "ELSE";
-        case TRUE:               return "TRUE";
-        case FALSE:              return "FALSE";
-        case FUNCTION:           return "FUNCTION";
-        case FOR:                return "FOR";
-        case WHILE:              return "WHILE";
-        case RETURN:             return "RETURN";
-        case LET:                return "LET";
-
-        case INVALID:            return "INVALID";
-
-        default:                 return "UNKNOWN_TOKEN";
+static TokenType str_to_token(const char* tok_str) {
+  for (size_t i = 0; i < TOKEN_TYPE_COUNT; i++) {
+    if (strcmp(tok_str, TokenType_names[i]) == 0) {
+      return (TokenType)i;
     }
+  }
 }
 
 void print_token(Token* token) {
@@ -167,4 +147,21 @@ void print_token(Token* token) {
         token->column
     );
 }
+
+static const char* TokenType_names[] = {
+    "LEFT_PARENTHESIS", "RIGHT_PARENTHESIS", "LEFT_BRACE", "RIGHT_BRACE",
+    "MINUS", "PLUS", "STAR", "SLASH", "SEMICOLON", "QUOTES", "COMMA", "DOT",
+
+    "BANG", "BANG_EQUAL",
+    "EQUAL", "EQUAL_EQUAL",
+    "GREATER", "GREATER_EQUAL",
+    "LESS", "LESS_EQUAL",
+
+    "STRING", "LITERAL", "IDENTIFIER",
+
+    "AND", "OR", "NOT", "IF", "ELIF", "ELSE", "TRUE", "FALSE",
+    "FUNCTION", "FOR", "WHILE", "RETURN", "LET",
+
+    "INVALID"
+};
 
